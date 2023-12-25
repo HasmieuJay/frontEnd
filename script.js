2a33ba542009b907838372bb32a189326983ce28
HasmieuJay <153967652+HasmieuJay@users.noreply.github.com>
2023-12-25 20:13:45 +0000

    Still on variables (js)

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let message = "This is a javascript file";
 let user_name = prompt("Enter your name");
 
 const msg = "This is a message";
-let new_msg = "This is another message";
+let new_msg;
 
 function func(){
   console.log(user_name);
   console.log(msg);
-  console.log(new_msg);
+  console.log(new_msg)
 }
